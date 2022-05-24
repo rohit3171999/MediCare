2d2d80be587aa2552288aa189729222677fcacf3
Rohit Sharma <rohit3171999@gmail.com>
2022-05-24 12:14:16 +0530

    How does it work section complete

--- medicare/src/App.js
+++ medicare/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./navbar";
-import Header from "./Header"
+import Header from "./Header";
+import HowItWorks from "./HowItWorks";
 const App = () => {
   return (
     <>
      <Navbar />
 {/* How to call navbar in App.Js */}
      <Header />
+     <HowItWorks />
     </>
   );
 };
---
+++ medicare/src/HowItWorks.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import workApi from "./API/workApi.js"
+const HowItWorks = () => {
+
+
+    const [workData, setWorkData]= useState(workApi);
+    console.log(workData);
+
+
+
+
+  return (
+    <>
+    <section>
+        <div className="work-container container">
+        {/* cotainer class decides the width of container is 80%  */}
+        <h1 className="main-heading text-center">How does it works</h1>
+        <div className="row">
+        {
+            workData.map((curEle) => {
+                const{id, logo, title, info}=curEle;
+                return (
+                    <>
+                    <div className="col-12 col-lg-4 text-center work-container-subdiv">
+        <i class={`sontawesome-style ${logo}`}></i>
+        {/* <i class={`sontawesome-style ${logo}`}></i> this is template string in new version in JS16 */}
+        <h2 className="sub-heading">{ title}</h2>
+        <p className="main-hero-para w-100">{ info}</p>
+        </div>
+                    </>
+                )
+
+            } )
+        }
+        {/* workData.map((curEle) => {} it is type of of a loop in react js repeat div when when curElement getting a new elemnet */}
+        {/* The map() function is used to iterate over an array and manipulate or change data items. 
+        In React, the map() function is most commonly used for rendering a list of data to the DOM. */}
+
+        
+
+        
+
+        </div>
+
+        </div>
+    </section>
+    </>
+  );
+};
+export default HowItWorks;
